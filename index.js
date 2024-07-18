d9b6e6aefbfd7ee8e2e2b6a7c427e5c4c78d45e4
jessekerch <jesse.kercheval@gmail.com>
2024-07-18 11:03:08 -0700

    fix: all routes now work with mongodb

--- index.js
+++ index.js
@@ -8,36 +8,25 @@ const Person = require('./models/person')
 
 const app = express()
 
-app.use(express.json())
-app.use(cors())
-
-morgan.token('body', (req) => {
-  return (req.method !== 'POST')
-  ? ''
-  : JSON.stringify(req.body)
-})
-
-app.use(morgan(':method :url :response-time :body'))
 app.use(express.static('dist'))
-
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
+app.use(express.json())
+app.use(morgan('tiny'))
+app.use(cors())
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
-app.get('/info', (request, response) => {
-  const count = persons.length
-  const now = new Date()
-
-  response.send(`
-    <p>Phonebook has info for ${count} people</p>
-    <p>${now}</p>
-    `)
+app.get('/info', (request, response, next) => {
+  Person.find({})
+  .then(persons => {
+    response.send(`
+      <p>Phonebook has info for ${persons.length} people</p>
+      <p>${new Date()}</p>
+      `)
+  })
+  .catch(error => next(error))
 })
-
 
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(persons => {
@@ -45,26 +34,20 @@ app.get('/api/persons', (request, response) => {
   })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    response.json(person)
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+  .then(person => {
+    if (person) {
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }
   })
+  .catch(error => next(error))
 })
-
-const generateId = () => {
-  let id = Math.ceil(Math.random() * 10000)
-  console.log('generating new id')
-  while (persons.find(p => p.id === String(id))) {
-    console.log('generating new id')
-    id = generateId()
-  }
-
-  return String(id)
-}
 
 app.post('/api/persons/', (request, response) => {
   const body = request.body
-  console.log('request body is: ', body)
   if (body.name === undefined) {
     return response
       .status(404)
@@ -81,34 +64,46 @@ app.post('/api/persons/', (request, response) => {
   })  
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  persons = persons.filter(p => p.id !== id)
-  response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      console.log('deleted person: ', result.name, result.number);
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
-app.put('/api/persons/:id', (request, response) => {
+app.put('/api/persons/:id', (request, response, next) => {
   const body = request.body
-  if (!body.name) {
-    return response.status(404)
-    .json({
-      error: 'put request name missing'
-    })
-  }
 
-  const id = body.id
-
-  const updatedPerson = {
-    id: id,
+  const person = {
     name: body.name,
     number: body.number
   }
 
-  persons = persons.map(p => p.id !== id ? p : updatedPerson)
-  response.json(updatedPerson)
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  .then(updatedPerson => {
+    response.json(updatedPerson)
+  })
+  .catch(error => next(error))
 })
 
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
 app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
